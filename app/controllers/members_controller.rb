d20746a9e4385e660d873770b3fe96409e0d7f57
Calvin Li <calvinl9331@gmail.com>
2015-06-04 14:42:15 -0400

    Member creation + deletion
    - edit form setup

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -3,13 +3,23 @@ class MembersController < ApplicationController
   def new
   end
 
-  def
+  def create
     @team = Team.find(params[:member][:team_id])
+    @team_name = @team.name
     @member = @team.members.build(member_params)
 
     respond_to do |format|
       @success = @member.save
-      flash[:success] = 'Member added.'
+      format.js
+    end
+  end
+
+  def destroy
+    @member = Member.find(params[:id])
+    @team = Team.find_by(id: @member.team_id).name
+    @success = @member.destroy
+
+    respond_to do |format|
       format.js
     end
   end
