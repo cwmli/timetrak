f16f089fd2fee327a95ca5e3e1c88b093fd3957e
Calvin Li <calvinl9331@gmail.com>
2015-06-07 18:38:18 -0400

    Email format changes

--- app/mailers/member_mailer.rb
+++ app/mailers/member_mailer.rb
@@ -5,7 +5,7 @@ class MemberMailer < ApplicationMailer
     @season_name = Season.find(team.season_id).title.to_s
     @members = Member.where(team_id: team.id)
     @events = team.events
-    @url = '/calendar?team_name='+Base64.encode64(@team_name)
+    @url = 'timetrak.herokuapp.com/calendar/view?team_name='+Base64.encode64(@team_name)
 
     @members.each do |m|
       mail(to: m.email, subject: @team_name+" Schedule for the "+@season_name+" Season")
