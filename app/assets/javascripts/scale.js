092e8acf772d87f24265309366d4c9912bde81e4
Calvin Li <calvinl9331@gmail.com>
2015-06-15 13:25:29 -0400

    season info view changes
    test schedule generations for 2,3,7,10 teams

--- app/assets/javascripts/scale.js
+++ app/assets/javascripts/scale.js
@@ -1,3 +1,6 @@
 $(document).ready(function(){
   $(".parting-block").css('border-left-width', screen.width);
+  $("#team-info").css('max-height', screen.height - 418);
+  $("#team-main").css('max-height', screen.height - 418);
+  $("#season-main").css('max-height', screen.height - 418);
 });
