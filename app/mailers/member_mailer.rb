173131d3cd925cab764286eb2164b13c4b53917f
Calvin Li <calvinl9331@gmail.com>
2015-06-18 16:06:17 -0400

    Revert "Push to heroku"
    This reverts commit 315e3e1bb03c3a3712d8326d446f01770fceccfd.

--- app/mailers/member_mailer.rb
+++ app/mailers/member_mailer.rb
@@ -1,14 +1,14 @@
 class MemberMailer < ApplicationMailer
 
   def schedule_email(team, season)
-    team_name = team.name.to_s
+    @team_name = team.name.to_s
     @season_name = Season.find(season).title.to_s
-    members = Member.where(team_id: team.id)
+    @members = Member.where(team_id: team.id)
     @events = team.events
-    @url = 'timetrak.herokuapp.com/calendar/view?team_name='+Base64.encode64(team_name)+'&season='+Base64.encode64(season.to_s)
+    @url = 'timetrak.herokuapp.com/calendar/view?team_name='+Base64.encode64(@team_name).to_s+'&season='+Base64.encode64(season).to_s
 
-    members.each do |m|
-      mail(to: m.email, subject: team_name+" Schedule for the "+@season_name+" Season") do |format|
+    @members.each do |m|
+      mail(to: m.email, subject: @team_name+" Schedule for the "+@season_name+" Season") do |format|
         format.html
         format.text
       end
